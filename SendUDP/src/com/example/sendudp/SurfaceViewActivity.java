2cf664a55de4a90d02b18c79b474dd35d7607ff2
markmclean5 <markmclean5@gmail.com>
2014-12-05 22:50:43 -0500

    Enhanced gauge needle, modified SurfaceViewActivity layout

--- SendUDP/src/com/example/sendudp/SurfaceViewActivity.java
+++ SendUDP/src/com/example/sendudp/SurfaceViewActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
@@ -37,6 +38,7 @@ public class SurfaceViewActivity extends Activity implements Runnable {
       @Override
       public void onClick(View arg0) {
         /** SWAP ANIMATION LEFT OR RIGHT CIRCLE*/
+    	 needleAngle = 0;
       }
     });
     }
@@ -77,50 +79,88 @@ public class SurfaceViewActivity extends Activity implements Runnable {
     
    
     // Gauge properties
-    int radius = height/2; 
-    int center_x = radius; // small offsets from sides
+    int radius = height/2-10; 
+    int center_x = radius + 10; // small offsets from sides
     int center_y = height/2;
     int tick_length = 40;
     
-    // Left gauge
+    // Draw gauge outer border
     paint.setColor(Color.BLACK);
     paint.setStrokeWidth(10);
     paint.setStyle(Paint.Style.STROKE);
     canvas.drawCircle(center_x, center_y, radius, paint);
-    paint.setStrokeWidth(5);
+    
+    //Draw gauge needle center
+    int needleWidth = 30;
+    paint.setStyle(Paint.Style.FILL);
+    paint.setColor(Color.RED);
+    canvas.drawCircle(center_x, center_y, needleWidth/2, paint);
     
     
     for(int angle = 0; angle < 360; angle+=15) {
+    	
+    	// Draw tick marks
     	paint.setStyle(Paint.Style.STROKE);
-    	int start_x = center_x + (int) ((radius-tick_length)*Math.cos(Math.toRadians((double)angle)));
+    	paint.setColor(Color.BLACK);
+    	paint.setStrokeWidth(5);
+    	int start_x = center_x - (int) ((radius-tick_length)*Math.cos(Math.toRadians((double)angle)));
     	int start_y = center_y - (int) ((radius-tick_length)*Math.sin(Math.toRadians((double)angle)));
-    	
-    	int end_x = center_x + (int) (radius*Math.cos(Math.toRadians((double)angle)));
+    	int end_x = center_x - (int) (radius*Math.cos(Math.toRadians((double)angle)));
     	int end_y = center_y - (int) (radius*Math.sin(Math.toRadians((double)angle)));
-    	
     	canvas.drawLine(start_x, start_y, end_x, end_y, paint);
         
-    	int text_x = center_x - 35 + (int) (0.85*radius*Math.cos(Math.toRadians((double)angle)));
-    	int text_y = center_y - (int) (0.85*radius*Math.sin(Math.toRadians((double)angle)));
+    	// Draw tick mark labels
+    	int text_x = center_x - 35 - (int) (0.8*radius*Math.cos(Math.toRadians((double)angle)));
+    	int text_y = center_y - (int) (0.8*radius*Math.sin(Math.toRadians((double)angle)));
     	paint.setTypeface(Typeface.MONOSPACE);
     	paint.setStyle(Paint.Style.FILL);
         paint.setTextSize(40);
         canvas.drawText(Integer.toString(angle), text_x, text_y, paint);
     }
     
+    // Draw needle line
     int start_x = center_x;
     int start_y = center_y;
-    int end_x = center_x + (int) (.85*radius*Math.cos(Math.toRadians((double)needleAngle)));
-    int end_y = center_y - (int) (.85*radius*Math.sin(Math.toRadians((double)needleAngle)));
+    int end_x = center_x - (int) (.5*radius*Math.cos(Math.toRadians((double)needleAngle)));
+    int end_y = center_y - (int) (.5*radius*Math.sin(Math.toRadians((double)needleAngle)));
     paint.setColor(Color.RED);
-    paint.setStrokeWidth(15);
+    paint.setStrokeWidth(needleWidth);
 	canvas.drawLine(start_x, start_y, end_x, end_y, paint);
+	
+	// draw needle triangle;
+	
+	
+	paint.setStyle(Paint.Style.FILL);
+	int p1_x = end_x + (int) (needleWidth/2*Math.sin(Math.toRadians((double)needleAngle)));
+	int p1_y = end_y - (int) (needleWidth/2*Math.cos(Math.toRadians((double)needleAngle)));
+	
+	int p2_x = end_x - (int) (needleWidth/2*Math.sin(Math.toRadians((double)needleAngle)));
+	int p2_y = end_y + (int) (needleWidth/2*Math.cos(Math.toRadians((double)needleAngle)));
+	
+	int p3_x = center_x - (int) (.75*radius*Math.cos(Math.toRadians((double)needleAngle)));
+    int p3_y = center_y - (int) (.75*radius*Math.sin(Math.toRadians((double)needleAngle)));
+    
 
     
     
+    Path path = new Path();
+    path.reset();
+    paint.setStrokeWidth(3);
+    paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    path.setFillType(Path.FillType.EVEN_ODD);
+    path.moveTo(p1_x, p1_y);
+    path.lineTo(p2_x, p2_y);
+    path.lineTo(p3_x, p3_y);
+    path.lineTo(p1_x, p1_y);
+    path.close();
+
+    canvas.drawPath(path, paint);
     
+    
+    
+    
+	// Display frames per second in bottom left hand corner of surfaceView
     paint.setStyle(Paint.Style.FILL);
-
     paint.setColor(Color.BLACK);
     paint.setTypeface(Typeface.MONOSPACE);
     paint.setTextSize(80);
